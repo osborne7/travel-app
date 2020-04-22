6efd277e1c9941e986c96ced782bab555be2f4bc
osborne7 <eleanor.c.osborne@gmail.com>
2020-04-22 10:11:56 -0400

    Add clean webpack plugin to dev and prod modes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const { cleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,6 +20,15 @@ module.exports = {
         new htmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new cleanWebpackPlugin({
+            //simulate the removal of files
+            dry: true,
+            //write logs to console
+            verbose: true,
+            //auto remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
