3746745330b19ae4f3151dc717598dd4105a7503
osborne7 <eleanor.c.osborne@gmail.com>
2020-04-22 09:56:13 -0400

    Add webpack and babel

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,16 @@
-//just a template, these names will need to be changed
-//import js
-import { functionToBeNamed } from './js/app.js'
+// //just a template, these names will need to be changed
+// //import js
+// import { functionToBeNamed } from './js/app.js'
 
-//import styles
-//this style file will be deleted
-import './styles/style.scss'
-import './styles/base.scss'
-import './styles/header.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
+// //import styles
+// //this style file will be deleted
+// import './styles/style.scss'
+// import './styles/base.scss'
+// import './styles/header.scss'
+// import './styles/footer.scss'
+// import './styles/form.scss'
 
-//export js
-export {
-    functionToBeNamed,
-}
+// //export js
+// export {
+//     functionToBeNamed,
+// }
