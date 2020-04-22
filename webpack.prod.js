5fa6a0f38c5de17705f25a9e7b84a08457666c71
osborne7 <eleanor.c.osborne@gmail.com>
2020-04-22 10:06:43 -0400

    Make webpack configs and build scripts for production and development

---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        mode: 'production',
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
