e9dab6e54677549dff2c8dc744e140595cf9cfd9
osborne7 <eleanor.c.osborne@gmail.com>
2020-07-28 12:18:14 -0400

    Remove tie to localhost from PostData request

--- src/client/js/apis.js
+++ src/client/js/apis.js
@@ -118,8 +118,8 @@ function execute(e) {
         const precipitation = weather.data[0].pop;
         const highTemp = weather.data[0].max_temp;
         const lowTemp = weather.data[0].min_temp;
-        const port = process.env.PORT || 3000
-        apiData = postData('http://localhost:3000/add', {country: country, city: city, departureDate: departureDate, temp: temp, summaryDescription: summaryDescription, precipitation: precipitation, highTemp: highTemp, lowTemp: lowTemp, code: code, remainingDays: remainingDays});
+        apiData = postData('/add', {country: country, city: city, departureDate: departureDate, temp: temp, summaryDescription: summaryDescription, precipitation: precipitation, highTemp: highTemp, lowTemp: lowTemp, code: code, remainingDays: remainingDays});
+        // apiData = postData('http://localhost:3000/add', {country: country, city: city, departureDate: departureDate, temp: temp, summaryDescription: summaryDescription, precipitation: precipitation, highTemp: highTemp, lowTemp: lowTemp, code: code, remainingDays: remainingDays});
         return apiData;
     }).then((apiData) => {
         updateUI(apiData);
